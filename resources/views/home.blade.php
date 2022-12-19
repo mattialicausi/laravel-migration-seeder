7d7bcd9b99e8a5d3d01c8e95264fd53941d47f08
mattialicausi <licausivitomattia@gmail.com>
2022-12-19 20:34:38 +0100

    Modificato footer, aggiunto icone social

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,26 +3,26 @@
 
 @section('content')
 
-<div class="row">
+<div class="row py-5">
 
     @foreach ($trains as $train)
 
     <div class="col-3 m-3">
 
         <h3 class="text-primary">{{$train->enterprise}}</h3>
-        <div>Luogo di Partenza: {{$train->departure_station}}</div>
-        <div>Luogo di arrivo: {{$train->arrival_station}}</div>
-        <div>Data del viaggio: {{$train->date}}</div>
-        <div>Orario di partenza: {{$train->departure_time}}</div>
-        <div>Codice treno: {{$train->train_code}}</div>
-        <div>Numero di carrozze: {{$train->n_carriages}}</div>
+        <div>Luogo di Partenza: <span class="text-success">{{$train->departure_station}}</span></div>
+        <div>Luogo di arrivo: <span class="text-success">{{$train->arrival_station}}</span></div>
+        <div>Data del viaggio: <span class="text-success">{{$train->date}}</span></div>
+        <div>Orario di partenza: <span class="text-success">{{$train->departure_time}}</span></div>
+        <div>Codice treno: <span class="text-success">{{$train->train_code}}</span></div>
+        <div>Numero di carrozze: <span class="text-success">{{$train->n_carriages}}</span></div>
 
         @if ($train->in_time = 1 ? 'Si' : 'No')
-            <div>In tempo: {{$train->in_time}}</div>
+            <div>In tempo: <span class="text-success">{{$train->in_time}}</span></div>
         @endif
 
         @if ($train->deleted = 1 ? 'Si' : 'No')
-            <div>Cancellato: {{$train->deleted}}</div>
+            <div>Cancellato: <span class="text-success">{{$train->deleted}}</span></div>
         @endif
 
     </div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,16 @@
 <footer class="p-4 bg-primary text-white">
 
-    <div class="footer-content">
+    <div class="footer-content d-flex justify-content-between align-items-center">
+
         <h5>Made by Mattia Li Causi</h5>
+
+
+        <div class="container-icon d-flex align-items-center">
+            <i class="fa-brands fa-facebook  mx-2"></i>
+            <i class="fa-brands fa-square-instagram mx-2"></i>
+            <i class="fa-brands fa-twitter mx-2"></i>
+        </div>
+
     </div>
 
 </footer>

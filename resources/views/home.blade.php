08a05be25f3b78b3320683bc3e9810c7b5203d37
mattialicausi <licausivitomattia@gmail.com>
2022-12-19 20:14:47 +0100

    Correzioni in home.blade.php per la visualizzazione dei dati, modifiche di stile, aggiunto navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,28 +3,26 @@
 
 @section('content')
 
-<h1>Main</h1>
-
 <div class="row">
 
     @foreach ($trains as $train)
 
-    <div class="col-3">
+    <div class="col-3 m-3">
 
-        <div>{{$train->enterprise}}</div>
-        <div>{{$train->departure_station}}</div>
-        <div>{{$train->date}}</div>
-        <div>{{$train->departure_time}}</div>
-        <div>{{$train->arrival}}</div>
-        <div>{{$train->train_code}}</div>
-        <div>{{$train->n_carriages}}</div>
+        <h3 class="text-primary">{{$train->enterprise}}</h3>
+        <div>Luogo di Partenza: {{$train->departure_station}}</div>
+        <div>Luogo di arrivo: {{$train->arrival_station}}</div>
+        <div>Data del viaggio: {{$train->date}}</div>
+        <div>Orario di partenza: {{$train->departure_time}}</div>
+        <div>Codice treno: {{$train->train_code}}</div>
+        <div>Numero di carrozze: {{$train->n_carriages}}</div>
 
-        @if ($train->n_carriages == true ? 'Si' : 'No')
-            <div>{{$train->n_carriages}}</div>
+        @if ($train->in_time = 1 ? 'Si' : 'No')
+            <div>In tempo: {{$train->in_time}}</div>
         @endif
 
-        @if ($train->n_carriages == true ? 'Si' : 'No')
-            <div>{{$train->deleted}}</div>
+        @if ($train->deleted = 1 ? 'Si' : 'No')
+            <div>Cancellato: {{$train->deleted}}</div>
         @endif
 
     </div>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,7 @@
-<footer>
-    <div>
-        Sono il footer
+<footer class="p-4 bg-primary text-white">
+
+    <div class="footer-content">
+        <h5>Made by Mattia Li Causi</h5>
     </div>
+
 </footer>

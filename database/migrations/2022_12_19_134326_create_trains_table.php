cc271e1a968bc59a6f4e0961d1345b6c1831d321
mattialicausi <licausivitomattia@gmail.com>
2022-12-19 15:55:42 +0100

    Aggiunto tabella con le colonne nel DB

---
+++ database/migrations/2022_12_19_134326_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('enterprise');
+            $table->string('departure_station');
+            $table->string('arrival_station');
+            $table->timeTz('departure_time');
+            $table->timeTz('arrival_time');
+            $table->string('train_code');
+            $table->integer('n_carriages');
+            $table->boolean('in_time');
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
